f778a2f079e370fe964d98683ed9afe357bd4b9e
kou-chan1125 <kouki.kunimoto@gmail.com>
2021-04-09 13:43:56 +0900

    商品購入機能の修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :soldout_move_to_index, only:  [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -55,5 +56,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to root_path unless current_user == @item.user
+  end
+
+  def soldout_move_to_index
+    redirect_to root_path if @item.order.present?
   end
 end
