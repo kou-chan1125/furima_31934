735ce28077d2c9a932f31e6181dff618d3cd18f1
kou-chan1125 <kouki.kunimoto@gmail.com>
2021-04-10 18:27:50 +0900

    Update gemfile.lock for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root "items#index"
-  resources :items do
-    resources :orders, only: [:index, :create]  
-  end
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
- 
